ea4f42a1134a4c2857f3ad9d03b8798ef270e447
urbanszabolcs <urbanszabolcs44@gmail.com>
2026-03-25 11:27:34 +0100

    Az alapvető input és main létrehozása

---
+++ main.c
@@ -0,0 +1,38 @@
+#define SDL_MAIN_HANDLED
+//
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+//
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//
+
+#include "src/input/input.h"
+//#include "src/camera/camera.h"
+//#include "src/models/model.h"
+//#include "src/terrain/terrain.h"
+
+//
+#define pi 3.142857
+
+//
+
+int main(int argc, char **argv)
+{
+   SDL_Init(SDL_INIT_VIDEO);
+   SDL_Window* win = SDL_CreateWindow("Teszt", 100, 100, 800, 700, SDL_WINDOW_OPENGL);
+   SDL_GLContext ctx = SDL_GL_CreateContext(win);
+   
+   glEnable(GL_DEPTH_TEST);
+   glMatrixMode(GL_PROJECTION);
+   gluPerspective(45.0, 800.0/700.0, 0.1, 1000.0);
+   glMatrixMode(GL_MODELVIEW);
+
+   //loadModel("assets/objects/player_model.obj");
+
+   
+}
---
+++ src/input/input.h
@@ -0,0 +1,18 @@
+#ifndef INPUT_H
+#define INPIT_H
+
+#include <SDL2/SDL.h>
+
+typedef struct{
+    int w,a,s,d;
+    int mouseLeftClick;
+    //int mouseRightClick;
+    int mouseXRel, mouseYRel;
+    int scroll;
+    //int shift;
+    //int esc;
+}InputState;
+
+void handleInput(SDL_Event* ev, InputState* state);
+
+#endif
