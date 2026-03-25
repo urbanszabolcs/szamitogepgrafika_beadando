a5c7bd41392f2e2248e379b1c3bc1c285c9ab61d
urbanszabolcs <urbanszabolcs44@gmail.com>
2026-03-25 13:03:21 +0100

    model.c

--- main.c
+++ main.c
@@ -23,56 +23,67 @@
 int main(int argc, char **argv)
 {
    SDL_Init(SDL_INIT_VIDEO);
-   SDL_Window* win = SDL_CreateWindow("Teszt", 100, 100, 800, 700, SDL_WINDOW_OPENGL);
+   SDL_Window *win = SDL_CreateWindow("Teszt", 100, 100, 800, 700, SDL_WINDOW_OPENGL);
    SDL_GLContext ctx = SDL_GL_CreateContext(win);
-   
+
    glEnable(GL_DEPTH_TEST);
    glMatrixMode(GL_PROJECTION);
-   gluPerspective(45.0, 800.0/700.0, 0.1, 1000.0);
+   gluPerspective(45.0, 800.0 / 700.0, 0.1, 1000.0);
    glMatrixMode(GL_MODELVIEW);
 
-   loadModel("assets/objects/player_model.obj");
+   loadModel("assets/models/player/player_model.obj");
 
    InputState input = {0};
-   float objX = 0, objZ = 0 , objAngle = 0;
+   float objX = 0, objZ = 0, objAngle = 0;
    float camAngleX = 20, camAngleY = 0, camDist = 20;
-   int running = 1; //Futás feltétele
+   int running = 1; // Futás feltétele
 
-   while (running) {
-        SDL_Event ev;
-        while (SDL_PollEvent(&ev)) {
-            if (ev.type == SDL_QUIT) running = 0;
-            handleInput(&ev, &input);
-        }
+   while (running)
+   {
+      SDL_Event ev;
+      while (SDL_PollEvent(&ev))
+      {
+         if (ev.type == SDL_QUIT)
+            running = 0;
+         handleInput(&ev, &input);
+      }
 
-        // Mozgás logika
-        if (input.w) { objX += sinf(objAngle*0.0174) * 0.2; objZ += cosf(objAngle*0.0174) * 0.2; }
-        if (input.a) objAngle += 2.0;
-        if (input.d) objAngle -= 2.0;
-        
-        camAngleY += input.mouseXRel * 0.5;
-        camAngleX += input.mouseYRel * 0.5;
-        camDist -= input.scroll * 2;
+      // Mozgás logika
+      if (input.w)
+      {
+         objX += sinf(objAngle * 0.0174) * 0.2;
+         objZ += cosf(objAngle * 0.0174) * 0.2;
+      }
+      if (input.s)
+      {
+         objX -= sinf(objAngle * 0.0174) * 0.2;
+         objZ -= cosf(objAngle * 0.0174) * 0.2;
+      }
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glLoadIdentity();
+      if (input.a)
+         objAngle += 2.0;
+      if (input.d)
+         objAngle -= 2.0;
 
-        updateCamera(objX, 0, objZ, camAngleX, camAngleY, camDist);
+      camAngleY += input.mouseXRel * 0.5;
+      camAngleX += input.mouseYRel * 0.5;
+      camDist -= input.scroll * 2;
 
-        drawWater(SDL_GetTicks() / 1000.0f);
-        
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      glLoadIdentity();
 
-        glPushMatrix();
-            glTranslatef(objX, 0, objZ);
-            glRotatef(objAngle, 0, 1, 0);
-            glColor3f(0.5, 0.3, 0.1);
-            renderModel();
-        glPopMatrix();
+      updateCamera(objX, 0, objZ, camAngleX, camAngleY, camDist);
 
-        SDL_GL_SwapWindow(win);
-        SDL_Delay(16);
-    }
-   
+      drawWater(SDL_GetTicks() / 1000.0f);
 
-   
+      glPushMatrix();
+      glTranslatef(objX, 0, objZ);
+      glRotatef(objAngle, 0, 1, 0);
+      glColor3f(0.5, 0.3, 0.1);
+      renderModel();
+      glPopMatrix();
+
+      SDL_GL_SwapWindow(win);
+      SDL_Delay(16);
+   }
 }
--- src/models/model.c
+++ src/models/model.c
@@ -1,20 +1,19 @@
 #include "model.h"
 #include <stdio.h>
 
-#define MAX_V 5000000
-
+#define MAX_V 500000
 Vertex vertices[MAX_V];
 int vertex_indices[MAX_V * 3];
 int index_count = 0;
 
-void loadModel(const char* path){
+void loadModel(const char* path) {
     FILE* f = fopen(path, "r");
-    if(!f) return;
+    if (!f) return;
     char line[128];
     int v_idx = 0;
     index_count = 0;
 
-     while (fgets(line, sizeof(line), f)) {
+    while (fgets(line, sizeof(line), f)) {
         if (line[0] == 'v' && line[1] == ' ') {
             sscanf(line, "v %f %f %f", &vertices[v_idx].x, &vertices[v_idx].y, &vertices[v_idx].z);
             v_idx++;
@@ -29,11 +28,11 @@ void loadModel(const char* path){
     fclose(f);
 }
 
-void renderModel(){
-    glbegin(GL_TRIANGLES);
-    for (int i = 0; i <index_count; i++){
+void renderModel() {
+    glBegin(GL_TRIANGLES);
+    for (int i = 0; i < index_count; i++) {
         Vertex v = vertices[vertex_indices[i]];
-        glVertex3f(v.x,v.y,v.z);
+        glVertex3f(v.x, v.y, v.z);
     }
     glEnd();
 }
